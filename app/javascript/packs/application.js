e13b48bb6ce0e12b49f02b41a4268077dfc56bbc
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-03-31 17:19:26 +0530

    Insatll semanti-ui & Add navigation menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// Loads all Semantic javascripts
+//= require semantic-ui
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("semantic-ui-sass")
+require("packs/custom")
 
 Rails.start()
 Turbolinks.start()
